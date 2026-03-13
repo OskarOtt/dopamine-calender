032de5dec75f51cedc58c2f56d803890e530c768
OskarOtt <oskar.ott@icloud.com>
2026-03-13 15:31:50 +0100

    minifixes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,16 @@
 import "../style/navStyle.scss"
 import {Link} from "react-router-dom";
 import { useGoals } from "../contexts/GoalsContext.tsx";
+import logo from "../assets/logo.jpg";
 
 const Nav = () => {
     const { setShowModal } = useGoals();
 
     return (
         <nav className="nav-container">
-            <div className="nav-logo">Logo</div>
+            <div className="nav-logo">
+                <img src={logo} alt={"logo"}/>
+            </div>
             <div className="nav-link-container">
                 <button className="add-goal-nav-button" onClick={() => setShowModal(true)}>Add Goal</button>
                 <Link className="link" to="/">Day</Link>
